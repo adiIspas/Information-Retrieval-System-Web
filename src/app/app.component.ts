471f35d7daaa0a9e520fe982993faee26736c620
Adrian Ispas <adrian.ispas@ab4.systems>
2018-03-14 00:05:00 +0200

    Make call from interface to backend and return results Design input box from page of search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'King Retrieval';
+  pageTitle = 'King Retrieval';
+  submit = 'Caută';
+  query: string;
+  results = {'content' : 'no-results'};
+
+  constructor(private http: HttpClient) {}
+
+  getResults() {
+    if (this.query !== null && this.query !== undefined && this.query.trim() !== '') {
+      this.http.get('http://localhost:8080/?query=' + this.query).subscribe(result => {
+        this.results = result;
+      });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -10,6 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    FormsModule,
+    HttpClientModule,
     BrowserModule
   ],
   providers: [],
